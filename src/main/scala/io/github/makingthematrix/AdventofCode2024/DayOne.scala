03ef86854b29273d78e579d32ec20043de75f2ea
Maciej Gorywoda <makingthematrix@protonmail.com>
2024-12-02 21:59:40 +0100

    more concise versions of Day 1 & 2 (but less readable)

--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DayOne.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DayOne.scala
@@ -4,22 +4,15 @@ import java.nio.file.{Files, Path}
 import scala.jdk.CollectionConverters.*
 import scala.util.matching.Regex
 
-object DayOne {
-  private val path = Path.of("resources/input1")
+object DayOne:
   private val linePattern: Regex = """^(\d+)\s+(\d+)$""".r
 
-  private def readInput: (Seq[Int], Seq[Int]) = {
-    val pairs = Files.readAllLines(path).asScala.toSeq.collect { case linePattern(a, b) => (a.toInt, b.toInt) }
+  private def readInput: (Seq[Int], Seq[Int]) =
+    val pairs =
+      Files.readAllLines(Path.of("resources/input1")).asScala.toSeq.collect { case linePattern(a, b) => (a.toInt, b.toInt) }
     (pairs.map(_._1), pairs.map(_._2))
-  }
 
-  def main(): Unit = {
+  def main(): Unit =
     val (left, right) = readInput
-    // Phase 1
-    val phase1 = left.sorted.zip(right.sorted).map((a, b) => math.abs(a - b)).sum
-    println(s"Phase 1: $phase1") // 3569916
-    // Phase 2
-    val phase2 = left.map { a => a * right.count(_ == a) }.sum
-    println(s"Phase 2: $phase2") // 26407426
-  }
-}
+    println(s"Phase 1: ${left.sorted.zip(right.sorted).map((a, b) => math.abs(a - b)).sum}") // 3569916
+    println(s"Phase 2: ${left.map { a => a * right.count(_ == a) }.sum}") // 26407426
--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DayTwo.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DayTwo.scala
@@ -4,56 +4,17 @@ import java.nio.file.{Files, Path}
 import scala.jdk.CollectionConverters.*
 import math.{abs, signum}
 
-object DayTwo {
-  private val path = Path.of("resources/input2")
-
+object DayTwo:
   private def readInput: Seq[Seq[Int]] =
-    Files.readAllLines(path).asScala.toSeq.map {
-      _.split(" ").toSeq.map(n => n.trim.toInt)
-    }
+    Files.readAllLines(Path.of("resources/input2")).asScala.toSeq.map { _.split(" ").toSeq.map(n => n.trim.toInt) }
 
-  private def findDir(seq: Seq[Int]): Option[Int] =
-    if (seq.length < 2) None
-    else if(seq.length == 3 && seq(0) != seq(1)) Some(signum(seq(1) - seq(0)))
-    else {
-      val dirs = (0 to 2).map(i => signum(seq(i + 1) - seq(i)))
-      if (dirs.count(_ == 1)>= 2) Some(1)
-      else if (dirs.count(_ == -1)>= 2) Some(-1)
-      else None
-    }
-
-  private def isSafe(seq: Seq[Int], useDampener: Boolean = false): Boolean = findDir(seq) match {
-    case None => false
-    case Some(dir) =>
-      val errors = seq.zip(seq.tail).zipWithIndex.collect {
-        case ((a, b), i) if dir == 1 && a >= b  => i
-        case ((a, b), i) if dir == -1 && a <= b => i
-        case ((a, b), i) if abs(b - a) > 3      => i
-      }
-      if (errors.isEmpty) true
-      else if (!useDampener) false
-      else
-        errors.exists { e =>
-          isSafe(seq.take(e) ++ seq.drop(e + 1)) ||
-            isSafe(seq.take(e + 1) ++ seq.drop(e + 2))
-        }
-  }
-
-  private def isSafeSimple(seq:Seq[Int], useDampener: Boolean): Boolean = {
+  private def isSafe(seq:Seq[Int], useDampener: Boolean): Boolean =
     val dir = signum(seq(1) - seq(0))
     val check = seq.zip(seq.tail).forall((a, b) => a != b && signum(b - a) == dir && abs(b - a) <= 3)
     if (check || !useDampener) check
-    else seq.indices.view.map(i => seq.take(i) ++ seq.drop(i + 1)).exists(isSafeSimple(_, false))
-  }
+    else seq.indices.view.map(i => seq.take(i) ++ seq.drop(i + 1)).exists(isSafe(_, false))
 
-  @main def main(): Unit = {
+  @main def main(): Unit =
     val seqs = readInput
-    // Part 1
-    val res1 = seqs.count(isSafeSimple(_, false))
-    println(res1) // 220
-
-    // Part 2
-    val res2 = seqs.count(isSafeSimple(_, true))
-    println(res2) // 296
-  }
-}
+    println(s"Part 1: ${seqs.count(isSafe(_, false))}") // 220
+    println(s"Part 2: ${seqs.count(isSafe(_, true))}") // 296
