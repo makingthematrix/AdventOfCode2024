475e7290938e53b81f76a0d15b086f6228e5595f
Maciej Gorywoda <makingthematrix@protonmail.com>
2024-12-04 20:46:41 +0100

    Day Four: a small optimisation

--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DayFour.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DayFour.scala
@@ -3,7 +3,7 @@ package io.github.makingthematrix.AdventofCode2024
 import io.github.makingthematrix.readLines
 
 object DayFour:
-  private val XmasArray  = Array('X', 'M', 'A', 'S')
+  private val XMasArray  = Array('X', 'M', 'A', 'S')
   private val XMasRange  = 0 to 3
   private val Directions = for { x <- -1 to 1; y <- -1 to 1 if (x, y) != (0, 0) } yield (x, y)
 
@@ -11,7 +11,7 @@ object DayFour:
     if x < 0 || y < 0 || x >= len || y >= len then None else Some(array(x * len + y))
 
   private def checkXMAS(x: Int, y: Int, dir: (Int, Int))(using input: Array[Char], len: Int): Boolean =
-    XMasRange.forall { n => getChar(x + (n * dir._1), y + (n * dir._2)).contains(XmasArray(n)) }
+    XMasRange.forall(n => getChar(x + (n * dir._1), y + (n * dir._2)).contains(XMasArray(n)))
 
   private def checkXMAS2(x: Int, y: Int)(using input: Array[Char], len: Int): Boolean = {
     for
@@ -27,10 +27,11 @@ object DayFour:
     val lines                = readLines("input4")
     given array: Array[Char] = lines.mkString.toCharArray
     given len: Int           = lines.head.length
-    val range                = for { x <- 0 until len; y <- 0 until len } yield (x, y)
     // Part 1
-    val res1 = range.map { (x, y) => Directions.count(checkXMAS(x, y, _)) }.sum
+    val range1               = for { x <- 0 until len; y <- 0 until len } yield (x, y)
+    val res1                 = range1.map((x, y) => Directions.count(checkXMAS(x, y, _))).sum
     println(res1) // 2344
     // Part 2
-    val res2 = range.count(checkXMAS2)
+    val range2               = for { x <- 1 until len - 1; y <- 1 until len - 1 } yield (x, y)
+    val res2                 = range2.count(checkXMAS2)
     println(res2) // 1815
--- src/main/scala/io/github/makingthematrix/AdventofCode2024/package.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/package.scala
@@ -5,5 +5,5 @@ package object AdventofCode2024
 import java.nio.file.{Files, Path}
 import scala.jdk.CollectionConverters.*
 
-def readLines(fileName: String): Seq[String] = Files.readAllLines(Path.of(s"resources/$fileName")).asScala.toSeq
-def readString(fileName: String): String = Files.readString(Path.of(s"resources/$fileName"))
+inline def readLines(fileName: String): Seq[String] = Files.readAllLines(Path.of(s"resources/$fileName")).asScala.toSeq
+inline def readString(fileName: String): String = Files.readString(Path.of(s"resources/$fileName"))
