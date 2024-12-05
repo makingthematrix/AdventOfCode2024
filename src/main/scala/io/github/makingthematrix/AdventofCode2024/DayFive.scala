abee62fcbdea9bbf3294e5782664518aae794d81
Maciej Gorywoda <makingthematrix@protonmail.com>
2024-12-05 22:22:00 +0100

    Day 5: optimization

--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DayFive.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DayFive.scala
@@ -4,26 +4,20 @@ import io.github.makingthematrix.readLines
 import scala.annotation.tailrec
 
 object DayFive:
+  @tailrec private def fixUpdate(update: Array[Int], n: Int = 0)(using ruleSet: Set[(Int, Int)]): Array[Int] =
+    if n == update.length - 1 then update
+    else if ruleSet((update(n), update(n + 1))) then fixUpdate(update, n + 1)
+    else
+      val t = update(n); update(n) = update(n + 1); update(n + 1) = t // not very safe, but works...
+      fixUpdate(update, if n > 0 then n - 1 else 1)
+
+  private def sumMiddles(updates: Seq[Array[Int]]): Int = updates.map(update => update(update.length / 2)).sum
+
   @main def main(): Unit =
-    val lines = readLines("input5")
-    val rules = lines.takeWhile(_.nonEmpty).map { line =>
-      val pair = line.split("\\|")
-      (pair(0).toInt, pair(1).toInt)
-    }
-
-    val ruleSet              = rules.toSet
-    val updates              = lines.drop(rules.length + 1).map(_.split(",").map(_.toInt))
-    val (correct, incorrect) = updates.partition(update => update.view.zip(update.tail).forall(ruleSet(_)))
-
-    // Part 1
-    val res1 = correct.map(update => update(update.length / 2)).sum
-    println(res1) // 4872
-
-    // Part 2
-    @tailrec def swap(update: Array[Int], n: Int = 0): Array[Int] =
-      if n == update.length - 1 then update
-      else if ruleSet((update(n), update(n + 1))) then swap(update, n + 1)
-      else swap(update.take(n) ++ Array(update(n + 1), update(n)) ++ update.drop(n + 2), if n > 0 then n - 1 else 1)
-
-    val res2 = incorrect.map(swap(_)).map(update => update(update.length / 2)).sum
-    println(res2) // 5564
+    val lines                      = readLines("input5")
+    val rules                      = lines.takeWhile(_.nonEmpty).map(_.split("\\|")).collect { case Array(a, b) => (a.toInt, b.toInt) }
+    given ruleSet: Set[(Int, Int)] = rules.toSet
+    val updates                    = lines.drop(rules.length + 1).map(_.split(",").map(_.toInt))
+    val (correct, incorrect)       = updates.partition(update => update.view.zip(update.tail).forall(ruleSet))
+    println(s"Part 1: ${sumMiddles(correct)}") // 4872
+    println(s"Part 2: ${sumMiddles(incorrect.map(fixUpdate(_)))}") // 5564
