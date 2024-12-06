31d617b74c4ac75bb5247d05c94edfd7ca2a444f
Maciej Gorywoda <makingthematrix@protonmail.com>
2024-12-06 22:11:26 +0100

    Day Six: One more crazy optimization :)

--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DaySix.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DaySix.scala
@@ -5,38 +5,37 @@ import scala.annotation.tailrec
 import scala.collection.parallel.immutable.ParVector
 
 object DaySix:
-  private def getChar(x: Int, y: Int)(using array: Array[Char], len: Int): Option[Char] =
-    if x < 0 || y < 0 || x >= len || y >= len then None else Some(array(x * len + y))
+  private def getChar(x: Int, y: Int)(using arr: Array[Char], len: Int): Option[Char] =
+    if x < 0 || y < 0 || x >= len || y >= len then None else Some(arr(x * len + y))
 
-  @tailrec private def traverse1(x: Int, y: Int, dir: (Int, Int) = (-1, 0), uniques: Int = 0)(using array: Array[Char], len: Int): Int =
-    val newUniques = getChar(x, y) match
-      case Some('.') => array.update(x * len + y, 'X'); uniques + 1
-      case _         => uniques
+  @tailrec private def getPath(x: Int, y: Int, dir: (Int, Int) = (-1, 0), lst: List[(Int, Int)] = Nil)(using arr: Array[Char], len: Int): List[(Int, Int)] =
+    val updated = getChar(x, y) match
+      case Some('.') => arr.update(x * len + y, 'X'); (x, y) :: lst
+      case _         => lst
     getChar(x + dir._1, y + dir._2) match
-      case None                  => newUniques
-      case Some('.') | Some('X') => traverse1(x + dir._1, y + dir._2, dir, newUniques)
-      case Some('#')             => traverse1(x, y, (dir._2, -dir._1), newUniques)
+      case None                  => updated
+      case Some('.') | Some('X') => getPath(x + dir._1, y + dir._2, dir, updated)
+      case Some('#')             => getPath(x, y, (dir._2, -dir._1), updated)
 
   private val Marks = Map('#' -> '1', '1' -> '2', '2' -> '3', '3' -> '4')
 
-  @tailrec private def traverse2(x: Int, y: Int, dir: (Int, Int) = (-1, 0))(using array: Array[Char], len: Int): Boolean =
+  @tailrec private def isInfiniteLoop(x: Int, y: Int, dir: (Int, Int) = (-1, 0))(using arr: Array[Char], len: Int): Boolean =
     getChar(x + dir._1, y + dir._2) match
       case None      => false
-      case Some('.') => traverse2(x + dir._1, y + dir._2, dir)
+      case Some('.') => isInfiniteLoop(x + dir._1, y + dir._2, dir)
       case Some('4') => true
-      case Some(c)   => array.update((x + dir._1) * len + y + dir._2, Marks(c)); traverse2(x, y, (dir._2, -dir._1))
+      case Some(c)   => arr.update((x + dir._1) * len + y + dir._2, Marks(c)); isInfiniteLoop(x, y, (dir._2, -dir._1))
 
   @main def main(): Unit =
     val (array, len) = readLines("input6") match { case lines => (lines.mkString.toCharArray, lines.head.length) }
     val start        = array.indexOf('^')
-    val (x, y)       = (start / len, start % len)
+    val t@(x, y)     = (start / len, start % len)
     array.update(start, '.')
     // Part 1
-    val res1 = traverse1(x, y)(using array.clone(), len)
-    println(s"Part 1: $res1") // 5329
+    val path = getPath(x, y)(using array.clone(), len)
+    println(s"Part 1: ${path.length}") // 5329
     // Part 2
-    val res2 =
-      ParVector.fromSpecific(0 until array.length)
-        .collect { case i if i != start && array(i) == '.' => array.updated(i, '#') }
-        .count { arr => traverse2(x, y)(using arr, len) }
+    val res2 = ParVector.fromSpecific(path.toSet -- Set(t))
+      .map { (a, b) => array.updated(a * len + b, '#') }
+      .count { arr => isInfiniteLoop(x, y)(using arr, len) }
     println(s"Part 2: $res2") // 2162
