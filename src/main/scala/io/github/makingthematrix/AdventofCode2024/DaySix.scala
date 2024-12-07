6822014a10d3c3db3230e44dfb882671f589a08a
Maciej Gorywoda <makingthematrix@protonmail.com>
2024-12-07 10:15:09 +0100

    Day 6: No updates in place in Part 1 + cosmetic changes

--- src/main/scala/io/github/makingthematrix/AdventofCode2024/DaySix.scala
+++ src/main/scala/io/github/makingthematrix/AdventofCode2024/DaySix.scala
@@ -1,21 +1,17 @@
 package io.github.makingthematrix.AdventofCode2024
 
-import io.github.makingthematrix.{readLines, getChar}
+import io.github.makingthematrix.{getChar, readLines}
 import scala.annotation.tailrec
-import scala.collection.parallel.immutable.ParVector
-import scala.util.chaining.*
+import scala.collection.parallel.CollectionConverters.*
 
 object DaySix:
-  @tailrec
-  private def getPath(x: Int, y: Int, dir: (Int, Int) = (-1, 0), lst: List[(Int, Int)] = Nil)
-                     (using arr: Array[Char], len: Int): List[(Int, Int)] =
-    val updated = getChar(x, y) match
-      case Some('.') => arr.update(x * len + y, 'X'); (x, y) :: lst
-      case _         => lst
+  @tailrec private def getPath(x: Int, y: Int, dir: (Int, Int) = (-1, 0), uniques: Set[(Int, Int)] = Set.empty)
+                              (using arr: Array[Char], len: Int): Set[(Int, Int)] =
+    val updated = if getChar(x, y).contains('.') then uniques + ((x, y)) else uniques
     getChar(x + dir._1, y + dir._2) match
-      case None                  => updated
-      case Some('.') | Some('X') => getPath(x + dir._1, y + dir._2, dir, updated)
-      case Some('#')             => getPath(x, y, (dir._2, -dir._1), updated)
+      case None      => updated
+      case Some('.') => getPath(x + dir._1, y + dir._2, dir, updated)
+      case Some('#') => getPath(x, y, (dir._2, -dir._1), updated)
 
   private val Marks = Map('#' -> '1', '1' -> '2', '2' -> '3', '3' -> '4')
 
@@ -25,22 +21,18 @@ object DaySix:
       case None      => false
       case Some('.') => isInfiniteLoop(x + dir._1, y + dir._2, dir)
       case Some('4') => true
-      case Some(c)   =>
-        arr.update((x + dir._1) * len + y + dir._2, Marks(c))
-        isInfiniteLoop(x, y, (dir._2, -dir._1))
+      case Some(c)   => arr.update((x + dir._1) * len + y + dir._2, Marks(c)); isInfiniteLoop(x, y, (dir._2, -dir._1))
 
   @main def main(): Unit =
-    val (array, len) = readLines("input6").pipe { lines => (lines.mkString.toCharArray, lines.head.length) }
+    val (array, len) = readLines("input6") match { case lines => (lines.mkString.toCharArray, lines.head.length) }
     val start        = array.indexOf('^')
     val t@(x, y)     = (start / len, start % len)
     array.update(start, '.')
     // Part 1
-    val path = getPath(x, y)(using array.clone(), len)
-    println(s"Part 1: ${path.length}") // 5329
+    val path = getPath(x, y)(using array, len)
+    println(s"Part 1: ${path.size}") // 5329
     // Part 2
-    val res2 =
-      ParVector
-        .fromSpecific(path.toSet -- Set(t))
-        .map { (a, b) => array.updated(a * len + b, '#') }
-        .count { arr => isInfiniteLoop(x, y)(using arr, len) }
+    val res2 = (path - t).toArray.par
+      .map { (a, b) => array.updated(a * len + b, '#') }
+      .count { arr => isInfiniteLoop(x, y)(using arr, len) }
     println(s"Part 2: $res2") // 2162
